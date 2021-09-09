a4ed26d1861212257a552d3b107274d6ed0299d9
BeastBeno <awoussidietzbenony@gmail.com>
2021-09-08 20:44:39 -0400

    Refactoring

--- game-api/src/main/java/ca/ulaval/glo4002/game/controllers/TurnController.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/controllers/TurnController.java
@@ -1,10 +1,9 @@
 package ca.ulaval.glo4002.game.controllers;
 
-import ca.ulaval.glo4002.game.application.TourService;
-import ca.ulaval.glo4002.game.domain.Tour;
 
-import javax.inject.Inject;
-import javax.ws.rs.GET;
+import ca.ulaval.glo4002.game.application.TurnService;
+import ca.ulaval.glo4002.game.domain.Turn;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -13,22 +12,21 @@ import javax.ws.rs.core.Response;
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
-public class TourController {
-    private final Tour tour = new Tour();
-    private final TourService service = new TourService(tour);
+public class TurnController {
+    private final Turn turn = new Turn();
+    private final TurnService service = new TurnService(turn);
 
     @Path("/turn")
     @POST
-    public TurnResponse myService(){
+    public TurnResponse myService() {
         return new TurnResponse(service.turn());
     }
 
 
     @Path("/reset")
     @POST
-    public Response reset(){
+    public Response reset() {
         service.reset();
         return Response.ok().build();
     }
-
 }
--- game-api/src/main/java/ca/ulaval/glo4002/game/domain/Turn.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/domain/Turn.java
@@ -2,7 +2,7 @@ package ca.ulaval.glo4002.game.domain;
 
 import java.util.Random;
 
-public class Tour {
+public class Turn {
     private final int ID = new Random().nextInt(100);
 
     public int turn(){
