52810ca2e389695ee6a66a7c1ca40bd4794996d9
chris <christian.bonilla.1@ulaval.ca>
2021-09-18 14:44:05 -0400

    ajout get byName

--- game-api/src/main/java/ca/ulaval/glo4002/game/interfaces/rest/dinosaur/api/DinosaurResource.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/interfaces/rest/dinosaur/api/DinosaurResource.java
@@ -46,4 +46,14 @@ public class DinosaurResource {
 
         return Response.ok().entity(response).build();
     }
+
+    @Path("/dinosaurs/{name}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getDinosaur(String name) {
+
+        DinosaurDto dinosaur = dinosaurUseCase.getDinosaur(name);
+        DinosaurResponseItem response = dinosaurDtoAssembler.toResponse(dinosaur);
+        return Response.ok().entity(response).build();
+    }
 }
