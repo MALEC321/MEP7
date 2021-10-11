b9ec55ef0b35d027cfc2eda2f20e68874b8c2530
justindjeumene <hugues-justin.djeumene-tcheou.1@ulaval.ca>
2021-10-11 16:47:32 -0600

    Initial commit Unit Tests for ResourcesUseCase.

--- game-api/src/main/java/ca/ulaval/glo4002/game/controllers/resources/dtos/ResourceAssemblers.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/controllers/resources/dtos/ResourceAssemblers.java
@@ -6,16 +6,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ResourceAssemblers {
-
-//    public ResourceDto toDto(Resource resource) {
-//        ResourceDto dto = new ResourceDto();
-//        dto.qtyBurger = resource.getResources().get("Burgers").getQuantity();
-//        dto.qtySalad = resource.getResources().get("Salads").getQuantity();
-//        dto.qtyWater = resource.getResources().get("Water").getQuantity();
-//
-//        return dto;
-//    }
-
     public ResourceDto toDto(Resource resource) {
         ResourceDto dto = new ResourceDto();
         dto.qtyBurger = resource.getBurgersQuantity();
@@ -29,4 +19,4 @@ public class ResourceAssemblers {
         return resources.stream().map(this::toDto).collect(Collectors.toList());
     }
 
-    }
+}
