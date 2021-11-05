7877c995e310d95ba9bdcf2fca545dc3c3629297
justindjeumene <hugues-justin.djeumene-tcheou.1@ulaval.ca>
2021-11-04 20:18:48 -0600

    Code Review - TurnRepositoryInMemoryTest!

--- game-api/src/test/java/ca/ulaval/glo4002/game/repositories/turn/TurnRepositoryInMemoryTest.java
+++ game-api/src/test/java/ca/ulaval/glo4002/game/repositories/turn/TurnRepositoryInMemoryTest.java
@@ -14,9 +14,8 @@ import ca.ulaval.glo4002.game.domain.turn.Turn;
 import ca.ulaval.glo4002.game.domain.turn.TurnRepository;
 
 class TurnRepositoryInMemoryTest {
-
-    private TurnRepository turnRepository;
-    private UUID fakeTurnId = UUID.fromString("4321-34-56-78-987654");
+    private TurnRepositoryInMemory turnRepositoryInMemory;
+    final private UUID fakeTurnId = UUID.fromString("4321-34-56-78-987654");
 
     @Mock
     private Turn turn1;
@@ -24,53 +23,41 @@ class TurnRepositoryInMemoryTest {
     @Mock
     private Turn turn2;
 
-
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        this.turnRepository = new TurnRepositoryInMemory();
+        this.turnRepositoryInMemory = new TurnRepositoryInMemory();
     }
 
     @Test
-    void whenGetAturnWithCorrectId_thenRightTurnIsReturned() {
+    void givenATurn_whenFindingThatTurnById_thenThatTurnIsReturned() {
         UUID idTurn1 = setTurnMock1();
 
-        turnRepository.save(turn1);
+        turnRepositoryInMemory.save(turn1);
+        Turn turn = turnRepositoryInMemory.findById(idTurn1);
 
-        assertNotNull(turnRepository.findById(idTurn1));
-        assertEquals(turn1.getId(), turnRepository.findById(idTurn1).getId());
-    }
-
-    @Test
-    void quandRecupereUnTourAvecLebonId_alorsBonTourEstRetourner() {
-        UUID idTurn1 = setTurnMock1();
-
-        turnRepository.save(turn1);
-
-        assertEquals(turn1, turnRepository.findById(idTurn1));
+        assertNotNull(turn);
+        assertEquals(turn1.getId(), turnRepositoryInMemory.findById(idTurn1).getId());
     }
 
     @Test
     void whenGetOneTurnWithTheBadId_thenNoneTurnIsReturned() {
-        UUID idTurn1 = setTurnMock1();
+        turnRepositoryInMemory.save(turn1);
 
-        turnRepository.save(turn1);
-
-        assertNull(turnRepository.findById(fakeTurnId));
+        assertNull(turnRepositoryInMemory.findById(fakeTurnId));
     }
 
     @Test
-    void repoContainsTwoTurns_whenResetTurns_thenRepoContainsNoMoreTurn() {
+    void givenTurns_whenResetTurns_thenRepoContainsNoMoreTurn() {
         UUID idTurn1 = setTurnMock1();
         UUID idTurn2 = setTurnMock2();
 
-        turnRepository.save(turn1);
-        turnRepository.save(turn2);
+        turnRepositoryInMemory.save(turn1);
+        turnRepositoryInMemory.save(turn2);
+        turnRepositoryInMemory.reset();
 
-        turnRepository.reset();
-
-        assertNotEquals(turn1, turnRepository.findById(idTurn1));
-        assertNotEquals(turn2, turnRepository.findById(idTurn2));
+        assertNotEquals(turn1, turnRepositoryInMemory.findById(idTurn1));
+        assertNotEquals(turn2, turnRepositoryInMemory.findById(idTurn2));
     }
 
     private UUID setTurnMock1() {
