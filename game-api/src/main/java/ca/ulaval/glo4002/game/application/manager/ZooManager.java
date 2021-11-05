608a40c532d97cfed1f93648ffe1b1cb1a43131c
marcantoinegenest <marcantoinegenest@live.ca>
2021-11-05 13:02:49 -0400

    Update ZooManager.java
    Small fix

--- game-api/src/main/java/ca/ulaval/glo4002/game/application/manager/ZooManager.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/application/manager/ZooManager.java
@@ -4,6 +4,8 @@ import ca.ulaval.glo4002.game.domain.dinosaur.Dinosaur;
 import ca.ulaval.glo4002.game.domain.dinosaur.Herd;
 import ca.ulaval.glo4002.game.domain.resources.Eatable;
 
+import java.util.List;
+
 import static ca.ulaval.glo4002.game.domain.resources.ResourceType.BURGER;
 import static ca.ulaval.glo4002.game.domain.resources.ResourceType.SALAD;
 import static ca.ulaval.glo4002.game.domain.resources.ResourceType.WATER;
@@ -15,6 +17,10 @@ public class ZooManager {
             if (!eatable.removeResource(SALAD, dinosaur.calculateSaladsNeeds())) dinosaur.setStarving(true);
             if (!eatable.removeResource(BURGER, dinosaur.calculateBurgersNeeds())) dinosaur.setStarving(true);
             if (!eatable.removeResource(WATER, dinosaur.calculateWaterNeeds())) dinosaur.setStarving(true);
+
+            if (dinosaur.isStarving()) {
+                herd.remove(dinosaur);
+            }
         }
     }
 
