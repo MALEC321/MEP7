5a9a73c5a249374874b4b94f549d41c311914d32
MALEC321 <77861833+MALEC321@users.noreply.github.com>
2021-11-08 21:33:29 -0500

    corrige commentaires de PR

---
+++ game-api/src/main/java/ca/ulaval/glo4002/game/application/resources/ResourcesFactory.java
@@ -0,0 +1,15 @@
+package ca.ulaval.glo4002.game.application.resources;
+
+import ca.ulaval.glo4002.game.domain.resources.Resources;
+import ca.ulaval.glo4002.game.domain.resources.ResourceType;
+import ca.ulaval.glo4002.game.exceptions.types.InvalidResourceQuantityException;
+
+public class ResourceFactory {
+    public Resources create(ResourceType resourceType, int quantity) {
+        if (quantity < 0) {
+            throw new InvalidResourceQuantityException();
+        } else {
+            return new Resources(resourceType, quantity, resourceType.getExpiration());
+        }
+    }
+}
