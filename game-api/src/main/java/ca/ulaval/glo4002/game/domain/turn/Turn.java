50392c383df82d4a7cc34586b2083bcfca617156
Debonnaire Ngatchui <debonnairengatchui@gmail.com>
2021-11-15 20:40:13 -0500

    Fix turnUseCaseTest

--- game-api/src/main/java/ca/ulaval/glo4002/game/domain/turn/Turn.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/domain/turn/Turn.java
@@ -7,7 +7,11 @@ import ca.ulaval.glo4002.game.domain.actions.Action;
 public class Turn {
 
     private final TurnNumber turnNumber;
-    private final List<Action> actions;
+    private List<Action> actions;
+
+    public Turn( TurnNumber turnNumber){
+        this.turnNumber = turnNumber;
+    }
 
     public Turn(TurnNumber turnNumber, List<Action> actions) {
         this.turnNumber = turnNumber;
--- game-api/src/test/java/ca/ulaval/glo4002/game/application/turn/TurnUseCaseTest.java
+++ game-api/src/test/java/ca/ulaval/glo4002/game/application/turn/TurnUseCaseTest.java
@@ -7,81 +7,84 @@ import ca.ulaval.glo4002.game.domain.game.GameRepository;
 import ca.ulaval.glo4002.game.domain.resources.ResourcesDistributor;
 import ca.ulaval.glo4002.game.domain.dinosaur.Dinosaur;
 import ca.ulaval.glo4002.game.domain.dinosaur.HerdRepository;
-import ca.ulaval.glo4002.game.domain.resources.Pantry;
 import ca.ulaval.glo4002.game.domain.resources.PantryRepository;
-import ca.ulaval.glo4002.game.domain.turn.Turn;
 import ca.ulaval.glo4002.game.domain.turn.TurnFactory;
-import ca.ulaval.glo4002.game.domain.turn.TurnNumber;
-import ca.ulaval.glo4002.game.infrastructure.persistence.turn.GameRepositoryInMemory;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class TurnUseCaseTest {
 
-    @Mock
-    private PantryRepository resourceRepository;
-    @Mock
+    public static final int EXPECTED = 1;
+
     private HerdRepository herdRepository;
-    @Mock
     private ResourcesDistributor resourcesDistributor;
-    @Mock
-    private Pantry pantry;
-    @Mock
     private TurnFactory turnFactory;
-
-    private GameRepository gameRepository;
-    @Mock
-    private PantryRepository pantryRepository;
-    @Mock
     private ActionRepository actionRepository;
-    @Mock
+    private GameRepository gameRepository;
+    private PantryRepository pantryRepository;
     private ResourcesFactory resourcesFactory;
-
     private Dinosaur dinosaurTest;
-
-    @InjectMocks
     private TurnUseCase turnUseCase;
-
-    private GameRepositoryInMemory gameRepositoryInMemory;
-
-    Game game;
+    private Game game;
 
 
-
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.initMocks(this);
-        when(resourceRepository.findPantry()).thenReturn(pantry);
+    @Before
+    public void setUp() {
 
         dinosaurTest= new Dinosaur("DinoTest", 1, "m", "Ankylosaurus");
+        game = new Game();
+        gameRepository = mock(GameRepository.class);
+        turnFactory = mock(TurnFactory.class);
+        pantryRepository = mock(PantryRepository.class);
+        herdRepository = mock(HerdRepository.class);
+        actionRepository = mock(ActionRepository.class);
+        resourcesDistributor = mock(ResourcesDistributor.class);
+        resourcesFactory = mock(ResourcesFactory.class);
+        turnUseCase = new TurnUseCase(turnFactory, gameRepository, pantryRepository, herdRepository, actionRepository,resourcesDistributor, resourcesFactory);
 
-         game = gameRepositoryInMemory.findGame();
-    }
-
-        @Test
-    public void whenDinosaurIsStarving_ThenDinosaurShouldBeRemove() {
-        List<Dinosaur> dinosaurs = new ArrayList<>(List.of(dinosaurTest));
-        //when(zooManager.feedDinosaur(pantry, dinosaurTest));
-       // turnUseCase.feedDinosaursByDietType(dinosaurs);
-
-       // verify(dinosaurRepository, times(1)).remove(dinosaurTest);
     }
 
     @Test
    public void whenGetTurnNumber_thenReturnedCurrentTurnNumber(){
-//        when(gameRepository.findGame()).thenReturn(game);
-//
-//        assertEquals(1, game.currentTurnNumber());
+        when(gameRepository.findGame()).thenReturn(game);
+
+        turnUseCase.getTurnNumber();
+
+        assertEquals(EXPECTED, game.currentTurnNumber().nextTurnNumber().getNumber());
 
     }
+
+    @Test
+    public void whenResetGame_thenReturnedNewGame(){
+        turnUseCase.resetGame();
+
+        verify(gameRepository).deleteAll();
+    }
+
+    @Test
+    public void whenResetGame_thenReturnedDeletedPantry(){
+        turnUseCase.resetGame();
+
+        verify(pantryRepository).deleteAll();
+    }
+
+    @Test
+    public void whenResetGame_thenReturnedDeletedHerd(){
+        turnUseCase.resetGame();
+
+        verify(herdRepository).deleteAll();
+    }
+
+    @Test
+    public void whenResetGame_thenReturnedDeletedActions(){
+
+        turnUseCase.resetGame();
+
+        verify(actionRepository).deleteAll();
+
+    }
+
 }
