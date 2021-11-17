ec6e5060b0d7696f0d45c737c5456281e556c201
Debonnaire Ngatchui <debonnairengatchui@gmail.com>
2021-11-17 18:53:17 -0500

    Fix dtosControllersTest

--- game-api/src/main/java/ca/ulaval/glo4002/game/controllers/baby/dtos/BabyRequest.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/controllers/baby/dtos/BabyRequest.java
@@ -1,17 +1,15 @@
 package ca.ulaval.glo4002.game.controllers.baby.dtos;
 
 public class BabyRequest {
-    private String name;
-    private String fatherName;
-    private String motherName;
+    private final String name;
+    private final String fatherName;
+    private final String motherName;
 
     public BabyRequest(String name, String fatherName, String motherName) {
         this.name = name;
         this.fatherName = fatherName;
         this.motherName = motherName;
     }
-
-    public BabyRequest() {}
 
     public String getName() {
         return name;
---
+++ game-api/src/test/java/ca/ulaval/glo4002/game/controllers/baby/dtos/BabyDtoAssemblerTest.java
@@ -0,0 +1,47 @@
+package ca.ulaval.glo4002.game.controllers.baby.dtos;
+
+import ca.ulaval.glo4002.game.application.baby.dtos.BabyDto;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BabyDtoAssemblerTest {
+	BabyRequest babyRequest;
+	BabyDtoAssembler babyDtoAssembler;
+	BabyDto babyDto;
+	BabyCreationDto babyCreationDto;
+	BabyResponse babyResponse;
+
+	@Before
+	public void setup(){
+		babyDtoAssembler = new BabyDtoAssembler();
+		babyDto = new BabyDto("baby", "fatherName", "motherName");
+		babyRequest = new BabyRequest("baby", "fatherName", "motherName");
+		babyCreationDto = new BabyCreationDto("baby", "fatherName", "motherName");
+		babyResponse = new BabyResponse("baby", "fatherName", "motherName");
+	}
+
+	@Test
+	public void givenBabyRequest_whenFromRequest_thenReturnedNewBaby(){
+
+		babyDtoAssembler.fromRequest(babyRequest);
+
+		assertEquals(babyRequest.getName(), babyCreationDto.getName());
+		assertEquals(babyRequest.getFatherName(), babyCreationDto.getFatherName());
+		assertEquals(babyRequest.getMotherName(), babyCreationDto.getMotherName());
+	}
+
+
+	@Test
+	public void givenBabyDto_whenToResponse_thenReturnedNewBabyResponse(){
+
+		babyDtoAssembler.toResponse(babyDto);
+
+		assertEquals(babyDto.getName(), babyResponse.getName());
+		assertEquals(babyDto.getFatherName(), babyResponse.getFatherName());
+		assertEquals(babyDto.getMotherName(), babyResponse.getMotherName());
+	}
+
+
+}
