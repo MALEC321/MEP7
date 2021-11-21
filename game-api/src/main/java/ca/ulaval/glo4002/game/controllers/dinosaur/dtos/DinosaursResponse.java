f9f3eca4356335a7c5a352631fc74d5fbf5fa6ce
ariau <alexisdeclairebois@gmail.com>
2021-11-21 12:48:23 -0500

    Add DinoAssembler Tests + Refactor dinoResponse

--- game-api/src/main/java/ca/ulaval/glo4002/game/controllers/dinosaur/dtos/DinosaursResponse.java
+++ game-api/src/main/java/ca/ulaval/glo4002/game/controllers/dinosaur/dtos/DinosaursResponse.java
@@ -3,13 +3,13 @@ package ca.ulaval.glo4002.game.controllers.dinosaur.dtos;
 import java.util.List;
 
 public class DinosaursResponse {
-    private final List<DinosaurResponseItem> items;
+    private final List<DinosaurResponse> items;
 
-    public DinosaursResponse(List<DinosaurResponseItem> items) {
+    public DinosaursResponse(List<DinosaurResponse> items) {
         this.items = items;
     }
 
-    public List<DinosaurResponseItem> getItems() {
+    public List<DinosaurResponse> getItems() {
         return items;
     }
 }
