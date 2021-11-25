2c6408fa03b8db1ad0e798b8323badfd4285de51
ariau <alexisdeclairebois@gmail.com>
2021-11-24 21:16:41 -0500

    Update ResourcesGroupFactoryTest.java

--- game-api/src/test/java/ca/ulaval/glo4002/game/application/resources/ResourcesGroupFactoryTest.java
+++ game-api/src/test/java/ca/ulaval/glo4002/game/application/resources/ResourcesGroupFactoryTest.java
@@ -1,11 +1,30 @@
 package ca.ulaval.glo4002.game.application.resources;
 
+import ca.ulaval.glo4002.game.domain.resources.ResourcesGroup;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static ca.ulaval.glo4002.game.domain.resources.ResourceType.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ResourcesGroupFactoryTest {
+    private ResourcesGroupFactory resourcesGroupFactory;
+
     @BeforeEach
     void setUp() {
+        resourcesGroupFactory = new ResourcesGroupFactory();
+    }
+
+    @Test
+    public void foodQuantities_whenCreateResourceGroup_thenReturnResourceGroupWithCorrectQuantities() {
+        int qtyBurger = 2;
+        int qtySalad = 3;
+        int qtyWater = 10;
+
+        ResourcesGroup resourcesGroup = resourcesGroupFactory.create(2, 3, 10);
+
+        assertEquals(qtyBurger, resourcesGroup.getResourceQuantity(BURGER));
+        assertEquals(qtySalad, resourcesGroup.getResourceQuantity(SALAD));
+        assertEquals(qtyWater, resourcesGroup.getResourceQuantity(WATER));
     }
 }
